f660d1d7072f1eb45753d56b3e33ed153dcae591
MichalB <marchewa626@gmail.com>
2024-04-08 22:15:45 +0200

    added functions and loop

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-let choices = ["Rock", "Paper", "Scissors"];
+let choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * choices.length);
-  return choices[randomNumber];
+function playGame() {
+  function getPlayerChoice() {
+    let playerChoice = prompt("rock, paper or scissors?").toLowerCase();
+    if (
+      playerChoice === "rock" ||
+      playerChoice === "paper" ||
+      playerChoice === "scissors"
+    ) {
+      return playerChoice;
+    } else {
+      console.log("Please type rock, paper or scissors");
+    }
+  }
+
+  function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+  }
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+
+  function playRound(playerSelection, computerSelection) {
+    if (
+      (playerSelection === "paper" && computerSelection === "paper") ||
+      (playerSelection === "rock" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "scissors")
+    ) {
+      console.log("It's a draw");
+    } else if (
+      (playerSelection === "scissors" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "rock" && computerSelection === "scissors")
+    ) {
+      console.log("You Win");
+    } else if (
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+      console.log("You Loose");
+    }
+  }
+
+  console.log(playerSelection, computerSelection);
+  playRound(playerSelection, computerSelection);
 }
 
-for(let i = 0; i = 10, i++;){
-console.log(getComputerChoice())
-
+for (let i = 0; i <= 5; i++) {
+  playGame();
 }
