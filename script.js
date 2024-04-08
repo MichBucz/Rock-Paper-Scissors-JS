0f4dc13db39ed60a8dafe2a2eb8f63578ab0e66c
MichalB <marchewa626@gmail.com>
2024-04-08 19:49:33 +0200

    clone

---
+++ script.js
@@ -0,0 +1,11 @@
+let choices = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * choices.length);
+  return choices[randomNumber];
+}
+
+for(let i = 0; i = 10, i++;){
+console.log(getComputerChoice())
+
+}
